886ebee4d8b2ea93ac74f946e723b72f0dabe5ac
Robert Guo <robert.guo@mongodb.com>
2021-06-11 06:28:36 -0400

    remove recordings

--- buildscripts/resmokelib/testing/testcases/cpp_unittest.py
+++ buildscripts/resmokelib/testing/testcases/cpp_unittest.py
@@ -35,9 +35,9 @@ class CPPUnitTestCase(interface.ProcessTestCase):
                     "Failed test run was recorded. For instructions on using the recording, "
                     "see: https://wiki.corp.mongodb.com/display/COREENG/Time+Travel+Debugging+in+MongoDB"
                 )
-            # Archive any available recordings if there's any failure. It's possible a problem
-            # with the recorder will cause no recordings to be generated.
-            self._cull_recordings()
+                # Archive any available recordings if there's any failure. It's possible a problem
+                # with the recorder will cause no recordings to be generated.
+                self._cull_recordings()
             raise
         else:
             self._remove_recordings()
